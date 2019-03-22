93a98eb6cf12ae12ea4f417e88c714e86cbc802f
root <root@USENGLEESJL3C.amer.dell.com>
2019-03-21 22:38:03 -0700

    two new hash map libraries with different collision strategies.

---
+++ Linked_List/hash_map_ll.py
@@ -0,0 +1,41 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
+class HashMap:
+  def __init__(self, size):
+    self.size = size
+    self.array = [LinkedList() for i in range(size)]
+    
+  def hash(self, key):
+    return sum(key.encode())
+  
+  def compress(self, hash_code):
+    return hash_code % self.size
+    
+  def assign(self, key, value):
+    hash_code = self.hash(key)
+    array_index = self.compress(hash_code)
+    payload = Node([key, value])
+    list_at_array = self.array[array_index]
+    for item in list_at_array:
+      if item[0] == key:
+        item[1] == value
+        return
+    list_at_array.insert(payload)        
+      
+    
+  def retrieve(self, key):
+    hash_code = self.hash(key)
+    array_index = self.compress(hash_code)
+    list_at_index = self.array[array_index]
+    for item in list_at_index:
+      if item[0] == key:
+        return item[1]
+    return None
+    
+blossom = HashMap(len(flower_definitions))
+for flower in flower_definitions:
+  blossom.assign(flower[0], flower[1])
+  
+for flower in flower_definitions:
+  print(blossom.retrieve(flower[0]))
